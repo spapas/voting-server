cf4b4edb0d3bf0b9b1e9a1b970be0d5aa0c6f86d
Serafeim Papastefanos <spapas@gmail.com>
2015-11-12 21:52:12 +0200

    Server side ok

--- src/core.js
+++ src/core.js
@@ -1,5 +1,7 @@
 import {List, Map} from 'immutable';
 
+export const INITIAL_STATE = Map();
+
 export function setEntries(state, entries) {
   return state.set('entries', List(entries));
 }
@@ -31,7 +33,7 @@ export function next(state) {
 
 export function vote(state, entry) {
   return state.updateIn(
-    ['vote', 'tally', entry],
+    ['tally', entry],
     0,
     tally => tally + 1
   );
